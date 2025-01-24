314aef3f4429ddad1f9a55d007c17cde96d7e377
Nethma de silva <nethmavilhan@gmail.com>
2025-01-24 15:04:05 +0530

    Add all changes

--- backend/server.py
+++ backend/server.py
@@ -13,6 +13,15 @@ def get_products():
     connection.close()  # Added line to close the connection after use
     return response
 
+@app.route('/deleteproducts',methods=['POST'])
+def delete_product():
+    return_id=products_dao.delete_product(connection,request.form['product_id'])
+    response = jsonify({
+        'product_id':return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
 
 if __name__=="__main__":
     print("Starting Python Flask Server For NVDS Grocery store Managemeny System")
