b7d61d62d2a33e662a4dcbf4481d53eced1e0cd2
Nethma de silva <nethmavilhan@gmail.com>
2025-01-25 07:54:50 +0530

    Add all updatates

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import products_dao
 from sql_connection import get_sql_connection
-
+import uom_dao
+import json
 app = Flask(__name__)
+
+connection = get_sql_connection()
 
 @app.route('/getproducts', methods=['GET'])
 def get_products():
@@ -13,11 +16,29 @@ def get_products():
     connection.close()  # Added line to close the connection after use
     return response
 
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response=jsonify(response)
+    response.headers.add('Access-Control-Aallow-Origin','*')
+    return response
+
 @app.route('/deleteproducts',methods=['POST'])
 def delete_product():
     return_id=products_dao.delete_product(connection,request.form['product_id'])
     response = jsonify({
         'product_id':return_id
+    })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/insertProduct',methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id':product_id
     })
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
